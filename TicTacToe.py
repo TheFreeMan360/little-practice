0a3b6ebb4c39068e7eebf420a3f883d341a3f929
TheFreeMan360 <54360yes@gmail.com>
2023-10-06 15:41:30 +0800

    add game object

---
+++ TicTacToe.py
@@ -0,0 +1,27 @@
+class Player:
+    def __init__(self) -> None:
+        pass
+
+    def move(grid:list[list])->list:
+        pass
+
+class HumanPlayer(Player):
+    pass
+
+class ComputerPlayer(Player):
+    pass
+
+class Game:
+    def __init__(self) -> None:
+        pass
+
+    def start():
+        pass
+
+    def check():
+        pass
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.start()
