8280768c7c2fe0cec43f0ce44f01517fe19f4e89
TheFreeMan360 <54360yes@gmail.com>
2023-11-18 22:55:11 +0800

    update new answer

---
+++ leetcode/171. Excel Sheet Column Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int sum = 0;
+        for(char e : columnTitle.toCharArray()){
+            sum *= 26;
+            sum += ((int)e - 64);
+        }
+        return sum;
+    }
+}
