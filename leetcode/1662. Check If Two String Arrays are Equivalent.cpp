c5acec403e702856bb72a1d07cc73fb942330232
TheFreeMan360 <54360yes@gmail.com>
2023-12-01 15:03:00 +0800

    [update] new answer

---
+++ leetcode/1662. Check If Two String Arrays are Equivalent.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string a = "", b = "";
+        for(string e : word1) a += e;
+        for(string e : word2) b += e;
+        return a == b;
+    }
+};
---
+++ leetcode/1662. Check If Two String Arrays are Equivalent.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuffer sb1 = new StringBuffer();
+        StringBuffer sb2 = new StringBuffer();
+
+        for(String e : word1) sb1.append(e);
+        for(String e : word2) sb2.append(e);
+
+        return sb1.toString().equals(sb2.toString());
+    }
+}
