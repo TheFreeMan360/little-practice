55925fb6846130520a3c48b2adb7d87b11b60176
TheFreeMan360 <54360yes@gmail.com>
2023-12-09 15:03:03 +0800

    Update new ans

---
+++ leetcode/0144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    vector<int> ans;
+
+    void perorder(TreeNode *root) {
+        if (!root)
+            return;
+        ans.push_back(root->val);
+        if (root->left)
+            perorder(root->left);
+        if (root->right)
+            perorder(root->right);
+    }
+
+    vector<int> preorderTraversal(TreeNode *root) {
+        perorder(root);
+        return ans;
+    }
+};
