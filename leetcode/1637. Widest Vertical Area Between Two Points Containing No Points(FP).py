91b9308ae9a2df553ca4d1c4d3107920ec794a6d
TheFreeMan360 <54360yes@gmail.com>
2023-12-13 23:17:50 +0800

    Fix logic

--- leetcode/1637. Widest Vertical Area Between Two Points Containing No Points(FP).py
+++ leetcode/1637. Widest Vertical Area Between Two Points Containing No Points(FP).py
@@ -1,4 +1,4 @@
 class Solution:
     def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
-        arr = sorted([p[0] for p in points])
+        arr = sorted([x for x, y in points])
         return max(list(map(lambda x, y: x - y, arr[1:], arr[:-1])))
