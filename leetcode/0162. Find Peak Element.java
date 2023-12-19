a598348b6038c0abfc0526a0fd3945d0d69e88c3
TheFreeMan360 <54360yes@gmail.com>
2023-12-19 23:58:31 +0800

    Update new ans

---
+++ leetcode/0162. Find Peak Element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while(l < r){
+            int mid = (r - l) / 2 + l;
+            if(nums[mid] < nums[mid + 1]) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+}
