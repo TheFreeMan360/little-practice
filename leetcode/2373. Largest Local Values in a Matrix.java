3939cf89e68ebd2f610983d402f863f7e87b11be
TheFreeMan360 <54360yes@gmail.com>
2023-12-19 21:05:24 +0800

    Fix logic

--- leetcode/2373. Largest Local Values in a Matrix.java
+++ leetcode/2373. Largest Local Values in a Matrix.java
@@ -2,29 +2,19 @@ class Solution {
     public int[][] largestLocal(int[][] grid) {
         int n = grid.length;
         int[][] ans = new int[n - 2][n - 2];
-        
-        for (int row = 1; row < n - 1; ++row){
-            for (int col = 1; col < n - 1; ++col){
+               
+        for (int row = 1; row < n - 1; ++row)
+            for (int col = 1; col < n - 1; ++col)
                 ans[row - 1][col - 1] = getLocalLagest(grid, row, col);
-            }
-        }
 
         return ans;
     }
 
     private int getLocalLagest(int[][] grid, int row, int col){
         int max = grid[row][col];
-        max = Math.max(max, grid[row - 1][col - 1]);
-        max = Math.max(max, grid[row - 1][col]);
-        max = Math.max(max, grid[row - 1][col + 1]);
-        
-        max = Math.max(max, grid[row][col - 1]);
-        max = Math.max(max, grid[row][col + 1]);
-
-        max = Math.max(max, grid[row + 1][col - 1]);
-        max = Math.max(max, grid[row + 1][col]);
-        max = Math.max(max, grid[row + 1][col + 1]);
-
+        for(int r = row - 1; r <= row + 1; ++r)
+            for(int c = col - 1; c <= col + 1; ++c)
+                max = Math.max(max, grid[r][c]);
         return max;
     }
 }
