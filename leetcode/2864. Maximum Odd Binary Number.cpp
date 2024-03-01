3d9986d46f2cabf75b269c396addba7e6063767d
TheFreeMan360 <54360yes@gmail.com>
2024-03-01 16:09:16 +0800

    Fix logic to more simple

--- leetcode/2864. Maximum Odd Binary Number.cpp
+++ leetcode/2864. Maximum Odd Binary Number.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        int n = s.size(), countOnes = 0;
-        for(int i = 0; i < n; ++i){
-            countOnes += s[i] - 48;
+        int ones = 0, zeros = 0;
+        for(const char& ch : s){
+            if(ch == '1') ones += 1;
+            if(ch == '0') zeros += 1;
         }
-        for(int i = 0; i < n - 1; ++i){
-            if(countOnes > 1){
-                --countOnes;
-                s[i] = '1';
-            }else{
-                s[i] = '0';
-            }
-        }
-        s[n - 1] = '1';
-        return s;
+
+        string ans = string(ones - 1, '1') + string(zeros, '0') + "1";
+        return ans;
     }
 };
